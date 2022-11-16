e715021b51dae6fcc4dcfafdaff429d2ab7e508b
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2022-11-16 11:47:17 +0300

    Работает отправка в фоне, есть обработка ответов

--- php/reg_obr.php
+++ php/reg_obr.php
@@ -3,24 +3,21 @@ header('Content-Type: text/html; charset=utf-8');
 
 $mysqli = mysqli_connect("localhost", "oefnbvtr_0975", "123456", "oefnbvtr_0975");
 if ($mysqli == false) {
-  print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+  print("error");
 } else {
-  print("Соединение установлено успешно");
+ // print("Соединение установлено успешно");
 
   $name = $_POST["name"];
   $lastname = $_POST["lastname"];
   $email = $_POST["email"];
   $pass = $_POST["pass"];
 
-  $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email' AND `pass`='$pass'");
 
   if ($result->num_rows != 0) {
-    print("Такой пользователь уже существует");
+    print("exist");
   } else {
     $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+    print("ok");
   }
-
-  //var_dump($result->num_rows);
-
-
 }
