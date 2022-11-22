ba066a78b852962a84d809e16b45c6d154963ec5
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2022-11-22 10:10:25 +0300

    add routes

--- Aroma2/template.php
+++ Aroma2/template.php
@@ -23,7 +23,7 @@
     <div class="main_menu">
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-          <a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+          <a class="navbar-brand logo_h" href="/"><img src="img/logo.png" alt=""></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -31,7 +31,7 @@
           </button>
           <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
             <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-              <li class="nav-item active"><a class="nav-link" href="index.php">Домашняя</a></li>
+              <li class="nav-item active"><a class="nav-link" href="/">Домашняя</a></li>
               <li class="nav-item submenu dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Магазин</a>
                 <ul class="dropdown-menu">
@@ -46,24 +46,24 @@
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Блог</a>
                 <ul class="dropdown-menu">
                   <li class="nav-item"><a class="nav-link" href="/blog">Блог</a></li>
-                  <li class="nav-item"><a class="nav-link" href="single-blog.html">Статьи</a></li>
+                  <li class="nav-item"><a class="nav-link" href="/articles">Статьи</a></li>
                 </ul>
               </li>
               <li class="nav-item submenu dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Для клиента</a>
                 <ul class="dropdown-menu">
-                  <li class="nav-item"><a class="nav-link" href="login.php">Вход</a></li>
-                  <li class="nav-item"><a class="nav-link" href="register.html">Регистрация</a></li>
-                  <li class="nav-item"><a class="nav-link" href="tracking-order.html">Отслеживание</a></li>
+                  <li class="nav-item"><a class="nav-link" href="/auth">Вход</a></li>
+                  <li class="nav-item"><a class="nav-link" href="/register">Регистрация</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">Отслеживание</a></li>
                 </ul>
               </li>
-              <li class="nav-item"><a class="nav-link" href="contact.html">Контакты</a></li>
+              <li class="nav-item"><a class="nav-link" href="/contact">Контакты</a></li>
             </ul>
 
             <ul class="nav-shop">
               <li class="nav-item"><button><i class="ti-search"></i></button></li>
               <li class="nav-item"><button><i class="ti-shopping-cart"></i><span class="nav-shop__circle">3</span></button> </li>
-              <li class="nav-item"><a class="button button-header" href="/login">Вход</a></li>
+              <li class="nav-item"><a class="button button-header" href="/auth">Вход</a></li>
             </ul>
           </div>
         </div>
